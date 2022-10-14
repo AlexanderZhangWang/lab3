377f061d0eff04fa20e388cf389e90dec86c6b83
AlexanderZhangWang <114326316+AlexanderZhangWang@users.noreply.github.com>
2022-10-13 17:38:57 -0700

    fixed filter and merge in ListExamples.java

---
+++ ListTests.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ListTests{
+
+    @Test
+    public void testFilter(){
+        ArrayList<String> arr = new ArrayList<>(); arr.add("a"); arr.add("aa"); arr.add("aaa"); arr.add("aaaa");
+        ArrayList<String> arr2 = new ArrayList<>(); arr2.add("aaa"); arr2.add("aaaa");
+
+        assertEquals(arr2, ListExamples.filter(arr, new ifLengthLargerThanTwo()));
+        
+    }
+
+    @Test
+    public void testMerge(){
+        ArrayList<String> arr = new ArrayList<>(); arr.add("a"); arr.add("c"); 
+        ArrayList<String> arr2 = new ArrayList<>(); arr2.add("b"); arr2.add("d"); arr2.add("e"); arr2.add("f");
+        ArrayList<String> arr3 = new ArrayList<>(); arr3.add("a"); arr3.add("b"); arr3.add("c"); arr3.add("d");
+         arr3.add("e"); arr3.add("f");
+
+        assertEquals(arr3, ListExamples.merge(arr, arr2));
+    }
+
+}
